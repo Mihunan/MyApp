2a15758db5eae1af8e3d5ecf6757f15b2975c250
mihunan <mihunanv.21@uom.lk>
2024-12-31 23:51:19 -0800

    index changed

--- AppNavigator.jsx
+++ AppNavigator.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import WelcomeScreen from './WelcomeScreen';
-import RegistrationScreen from './RegistrationScreen';
-import LoginScreen from './LoginScreen';
-import HomeScreen from './HomeScreen';
+import WelcomeScreen from './Screens/WelcomeScreen';
+import RegistrationScreen from './Screens/RegistrationScreen';
+import LoginScreen from './Screens/LoginScreen';
+import HomeScreen from './Screens/HomeScreen';
 
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
   return (
-    
+      <NavigationContainer>
       <Stack.Navigator initialRouteName="Welcome">
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
         <Stack.Screen name="Register" component={RegistrationScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
+      </NavigationContainer> 
+   
     
   );
 };
